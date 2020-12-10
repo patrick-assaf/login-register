28c6c8916ea2387a3e88f2ab5f15db90f712c63f
Patrick Assaf <patrickassaf96@gmail.com>
2020-12-10 16:16:08 +0200

    Remove unused links from index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+interface Props {
+
+}
+
+export const App: React.FC<Props> = () => {
   return (
     <h1>Sign In / Sign Up</h1>
   );
-}
-
-export default App;
+};
